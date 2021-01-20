539966edd733b88d1e4c7c9a5c0bfb49f1474d30
Silvio <silvio.brancati4@etu.univ-lorraine.fr>
2021-01-20 21:13:22 +0100

    Update wishna.sql

--- wishna.sql
+++ wishna.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : mer. 20 jan. 2021 à 20:46
+-- Généré le : mer. 20 jan. 2021 à 21:12
 -- Version du serveur :  10.4.17-MariaDB
 -- Version de PHP : 8.0.0
 
@@ -20,6 +20,69 @@ SET time_zone = "+00:00";
 --
 -- Base de données : `wishna`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `compte`
+--
+
+CREATE TABLE `compte` (
+  `idCompte` int(11) NOT NULL,
+  `userName` varchar(36) DEFAULT NULL,
+  `password` varchar(36) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `compte`
+--
+
+INSERT INTO `compte` (`idCompte`, `userName`, `password`) VALUES
+(1, 'Jean', 'jean123');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `item`
+--
+
+CREATE TABLE `item` (
+  `id` int(11) NOT NULL,
+  `liste_id` int(11) NOT NULL,
+  `nom` text NOT NULL,
+  `descr` text DEFAULT NULL,
+  `img` text DEFAULT NULL,
+  `url` text DEFAULT NULL,
+  `tarif` decimal(5,2) DEFAULT NULL,
+  `reservation` tinyint(1) NOT NULL DEFAULT 0,
+  `nom_reservation` text DEFAULT NULL,
+  `message_reservation` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `item`
+--
+
+INSERT INTO `item` (`id`, `liste_id`, `nom`, `descr`, `img`, `url`, `tarif`, `reservation`, `nom_reservation`, `message_reservation`) VALUES
+(1, 2, 'Champagne', 'Bouteille de champagne + flutes + jeux à gratter', 'champagne.jpg', '', '20.00', 0, NULL, NULL),
+(2, 2, 'Musique', 'Partitions de piano à 4 mains', 'musique.jpg', '', '25.00', 0, NULL, NULL),
+(3, 2, 'Exposition', 'Visite guidée de l’exposition ‘REGARDER’ à la galerie Poirel', 'poirelregarder.jpg', '', '14.00', 0, NULL, NULL),
+(4, 3, 'Goûter', 'Goûter au FIFNL', 'gouter.jpg', '', '20.00', 0, NULL, NULL),
+(5, 3, 'Projection', 'Projection courts-métrages au FIFNL', 'film.jpg', '', '10.00', 0, NULL, NULL),
+(6, 2, 'Bouquet', 'Bouquet de roses et Mots de Marion Renaud', 'rose.jpg', '', '16.00', 0, NULL, NULL),
+(7, 2, 'Diner Stanislas', 'Diner à La Table du Bon Roi Stanislas (Apéritif /Entrée / Plat / Vin / Dessert / Café / Digestif)', 'bonroi.jpg', '', '60.00', 0, NULL, NULL),
+(8, 3, 'Origami', 'Baguettes magiques en Origami en buvant un thé', 'origami.jpg', '', '12.00', 0, NULL, NULL),
+(9, 3, 'Livres', 'Livre bricolage avec petits-enfants + Roman', 'bricolage.jpg', '', '24.00', 0, NULL, NULL),
+(10, 2, 'Diner  Grand Rue ', 'Diner au Grand’Ru(e) (Apéritif / Entrée / Plat / Vin / Dessert / Café)', 'grandrue.jpg', '', '59.00', 0, NULL, NULL),
+(11, 0, 'Visite guidée', 'Visite guidée personnalisée de Saint-Epvre jusqu’à Stanislas', 'place.jpg', '', '11.00', 0, NULL, NULL),
+(12, 2, 'Bijoux', 'Bijoux de manteau + Sous-verre pochette de disque + Lait après-soleil', 'bijoux.jpg', '', '29.00', 0, NULL, NULL),
+(19, 0, 'Jeu contacts', 'Jeu pour échange de contacts', 'contact.png', '', '5.00', 0, NULL, NULL),
+(22, 0, 'Concert', 'Un concert à Nancy', 'concert.jpg', '', '17.00', 0, NULL, NULL),
+(23, 1, 'Appart Hotel', 'Appart’hôtel Coeur de Ville, en plein centre-ville', 'apparthotel.jpg', '', '56.00', 0, NULL, NULL),
+(24, 2, 'Hôtel d\'Haussonville', 'Hôtel d\'Haussonville, au coeur de la Vieille ville à deux pas de la place Stanislas', 'hotel_haussonville_logo.jpg', '', '169.00', 0, NULL, NULL),
+(25, 1, 'Boite de nuit', 'Discothèque, Boîte tendance avec des soirées à thème & DJ invités      ', 'noImage.png', '', '32.00', 0, NULL, NULL),
+(26, 1, 'Planètes Laser', 'Laser game : Gilet électronique et pistolet laser comme matériel, vous voilà équipé.', 'laser.jpg', '', '15.00', 0, NULL, NULL),
+(27, 1, 'Fort Aventure', 'Découvrez Fort Aventure à Bainville-sur-Madon, un site Accropierre unique en Lorraine ! Des Parcours Acrobatiques pour petits et grands, Jeu Mission Aventure, Crypte de Crapahute, Tyrolienne, Saut à l\'élastique inversé, Toboggan géant... et bien plus encore.', 'fort.jpg', '', '25.00', 0, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -51,6 +114,18 @@ INSERT INTO `liste` (`no`, `titre`, `description`, `expiration`, `token`, `token
 --
 
 --
+-- Index pour la table `compte`
+--
+ALTER TABLE `compte`
+  ADD PRIMARY KEY (`idCompte`);
+
+--
+-- Index pour la table `item`
+--
+ALTER TABLE `item`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Index pour la table `liste`
 --
 ALTER TABLE `liste`
@@ -59,6 +134,18 @@ ALTER TABLE `liste`
 --
 -- AUTO_INCREMENT pour les tables déchargées
 --
+
+--
+-- AUTO_INCREMENT pour la table `compte`
+--
+ALTER TABLE `compte`
+  MODIFY `idCompte` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT pour la table `item`
+--
+ALTER TABLE `item`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
 
 --
 -- AUTO_INCREMENT pour la table `liste`
